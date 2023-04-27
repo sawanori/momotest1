79aeb6615ba7f8eafd05ec3ccac5b5789d199ff0
noritakasawada <snp.inc.info@gmail.com>
2023-04-27 14:13:07 +0900

    test12

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,37 @@ import React,  {useEffect, useState} from 'react'
 export default function App({ Component, pageProps }) {
   const [message, setMessage] = useState('テスト')
   const [error, setError] = useState('')
-
-
+ 
+  const [[liffObject, profile], setLiffState] = useState([null, null]);
   useEffect(() => {
-    const initLiff = async () => {
-      try {
-        const { default: liff } = await import('@line/liff');
-        await liff.load({ liffId: process.env.NEXT_PUBLIC_LIFF_ID });
-        setMessage('LIFF 初期化成功');
-      } catch (err) {
-        setMessage('LIFF 初期化エラー');
-        setError(err);
-      }
-    };
+    if (!pageProps.liffId) return;
+    import("@line/liff").then((liff) => {
+      liff
+        .init({ liffId: pageProps.liffId })
+        .then(() => {
+          if (liff.isLoggedIn()) {
+            liff
+              .getProfile()
+              .then((profile) => {
+                setLiffState([liff, profile]);
+              })
+              .catch((err) => {
+                console.warn({ err });
+              })
 
-    initLiff();
+          } else {
+            setLiffState([liff, null]);
+          }
+        })
+        .catch((err) => {
+          console.warn({ err });
+        });
+    });
   }, []);
 
+
+
+    console.log(message)
 
   return <Component {...pageProps} />
 }
